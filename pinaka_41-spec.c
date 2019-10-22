ca6ab0f2c6495ee72b7f0e844ae3b323e7796f7e
panipinaka <37842071+panipinaka@users.noreply.github.com>
2019-10-22 16:17:34 +0530

    Add files via upload

---
+++ pinaka_41-spec.c
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <assert.h>
+#include "heap.h"
+
+void test_sort( int32_t data[], uint32_t len)
+{
+	uint32_t idx;
+	for (idx = 1; idx <= len; ++idx) {
+		assert (data[idx] <= data[idx+1]);
+	}
+}
+
+void test_heap()
+{
+
+f data[] = { {0,167365,1},
+                            {1130,224678,0},
+                            {1030,247291,1},
+                            {1330,84202,0},
+                            {130,163191,0},
+                            {450,136190,1},
+                            {1654,234653,1}
+                            };
+
+	Heap heap = heap_new(data,8);
+	//heap_insert(&heap, 47);
+	//heap_test(&heap);
+	//assert(heap_get_max(&heap) == 93);
+	//assert(heap_extract_max(&heap) == 93);
+	//heap_test(&heap);
+	//assert(heap_size(&heap) == 10);
+
+	//heap_sort(&heap);
+	//test_sort(heap.data, 10);
+
+}
+
+int main()
+{
+	test_heap();
+	return 0;
+}
---
+++ pinaka_41.c
@@ -0,0 +1,128 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "heap.h"
+
+
+static void _swap_(f *data1, f *data2) {
+
+
+int32_t time_stamp = data1->time_stamp;
+int32_t plane_no = data1->plane_no;
+int32_t event = data1->event;
+
+data1->time_stamp = data2->time_stamp;
+data1->plane_no = data2->plane_no;
+data1->event = data2->event;
+
+data2->time_stamp = time_stamp;
+data2->plane_no = plane_no;
+data2->event = event;
+}
+
+static void _heapify_ (f data[], uint32_t len, uint32_t parent) {
+	assert (len > 0 && len < HEAP_MAX_SIZE && parent > 0 );
+
+	uint32_t child = 2 * parent;
+
+	while (child <= len) {
+		if (child + 1 <= len) {
+			if (data[child+1].time_stamp > data[child].time_stamp){
+				++child;
+			}
+		}
+		if (data[parent].time_stamp >= data[child].time_stamp) {
+			break;
+		}
+		_swap_(&data[parent].time_stamp, &data[child].time_stamp);
+		parent = child;
+		child = 2 * parent;
+	}
+}
+/*
+static void _test_heap_(int32_t data[], uint32_t len) {
+
+	for (uint32_t child = len; child > 1; --child) {
+		assert(data[child] <= data[child/2]);
+	}
+
+}*/
+/*
+Heap*	heap_test(Heap *heap)
+{
+	_test_heap_(heap->data, heap->size);
+}
+*/
+
+Heap heap_new(f data[], uint32_t len) {
+	assert (len > 0 && len < HEAP_MAX_SIZE);
+
+	Heap heap;
+	uint32_t idx = len/2;
+	for (idx = len/2; idx > 0; --idx) {
+		_heapify_(data, len, idx);
+	}
+	heap.size = len;
+	memcpy(heap.data, data, (len+1) * sizeof(f));
+	//_test_heap_(heap.data, heap.size);
+
+	return heap;
+}
+/*
+Heap*	heap_sort(Heap *heap) {
+	assert (heap->size > 0 && heap->size < HEAP_MAX_SIZE);
+
+	uint32_t idx = heap->size;
+
+	for (idx = heap->size; idx > 1; idx--) {
+		_swap_(&heap->data[idx], &heap->data[1]);
+		_heapify_(heap->data, idx-1, 1);
+	}
+
+	return heap;
+}
+
+
+Heap*	heap_insert(Heap *heap, int32_t key)
+{
+	assert (heap->size > 0 && heap->size+1 < HEAP_MAX_SIZE);
+
+	++heap->size;
+	uint32_t parent = heap->size/2;
+	uint32_t loc = heap->size;
+
+	while ( loc > 1 && heap->data[parent] < key) {
+		heap->data[loc] = heap->data[parent];
+		loc = parent;
+		parent = parent/2;
+	}
+	heap->data[loc] = key;
+	return heap;
+
+}
+
+int32_t		heap_get_max(Heap *heap)
+{
+	assert (heap->size > 0 && heap->size < HEAP_MAX_SIZE);
+	return heap->data[1];
+
+}
+
+int32_t		heap_extract_max(Heap *heap)
+{
+	assert (heap->size > 0 && heap->size < HEAP_MAX_SIZE);
+
+	int32_t max = heap->data[1];
+	_swap_(&heap->data[1], &heap->data[heap->size]);
+	--heap->size;
+	_heapify_(heap->data, heap->size, 1);
+
+	return max;
+}
+*/
+uint32_t	heap_size(Heap *heap)
+{
+	assert (heap->size > 0 && heap->size < HEAP_MAX_SIZE);
+	return heap->size;
+}
